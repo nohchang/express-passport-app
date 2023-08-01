29ef4bb50e131333a835f5b63d9ef5d2a495cf9d
nohchang <nchq2123@naver.com>
2023-08-01 19:54:07 +0900

    router refactoring

--- src/server.js
+++ src/server.js
@@ -4,8 +4,13 @@ const { default: mongoose } = require('mongoose');
 const passport = require('passport');
 const app = express();
 const path = require('path');
-const { checkAuthenticated, checkNotAuthenticated } = require('./middlewares/auth');
-const User = require('./models/users.model');
+
+const config = require('config');
+const mainRouter = require('./routes/main.router');
+const usersRouter = require('./routes/users.router');
+const serverConfig = config.get('server');
+
+const port = serverConfig.port;
 
 require('dotenv').config()
 
@@ -46,66 +51,9 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
-app.get('/', checkAuthenticated, (req, res) => {
-  res.render('index');
-})
+app.use('/', mainRouter);
+app.use('/auth', usersRouter);
 
-app.get('/login', checkNotAuthenticated, (req, res) => {
-  res.render('login');
-})
-
-app.post('/login', (req, res, next) => {
-  passport.authenticate("local", (err, user, info) => {
-    if (err) {
-      return next(err);
-    }
-
-    if (!user) {
-      return res.json({ msg: info });
-    }
-
-    req.logIn(user, function(err) {
-      if (err) { return next(err); }
-      res.redirect('/');
-    })
-  })(req, res, next)
-})
-
-app.post('/logout', (req, res, next) => {
-  req.logOut(function(err) {
-    if (err) { return next(err); }
-    res.redirect('/login');
-  })
-})
-
-app.get('/signup', checkNotAuthenticated, (req, res) => {
-  res.render('signup');
-})
-
-app.post('/signup', async (req, res) => {
-  // user 객체를 생성합니다.
-  const user = new User(req.body);
-  try {
-    // user 컬렉션에 유저를 저장합니다.
-    await user.save();
-    return res.status(200).json({
-      success: true
-    });
-  } catch (error) {
-    console.log(error);
-  }
-})
-
-app.get('/auth/google', passport.authenticate('google'));
-app.get('/auth/google/callback', passport.authenticate('google', {
-  successReturnToOrRedirect: '/',
-  failureRedirect: '/login'
-}))
-
-const config = require('config');
-const serverConfig = config.get('server');
-
-const port = serverConfig.port;
 app.listen(port, () => {
   console.log(`Listening on ${port}`);
 });
